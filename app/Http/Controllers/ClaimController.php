a76f8a0fc98492c1ac9827f244e2586864d31dfa
thanhtinh <thanhtinh030291@gmail.com>
2021-02-22 16:28:04 +0700

    update

--- app/Http/Controllers/ClaimController.php
+++ app/Http/Controllers/ClaimController.php
@@ -932,7 +932,7 @@ class ClaimController extends Controller
             }else{
                 $mpdf = new \Mpdf\Mpdf(['tempDir' => base_path('resources/fonts/'), 'margin_top' => 32]);
                 $mpdf->WriteHTML('
-                <div style="position: absolute; left: 55px; top: 55px;font-weight: bold; ">
+                <div style="position: absolute; left: 55px; top: 75px;font-weight: bold; ">
                     <img src="'.asset("images/logo-cathay.png").'" alt="head">
                 </div>');
                 $mpdf->WriteHTML(data_get($export_letter->approve, 'data'));
@@ -1236,7 +1236,7 @@ class ClaimController extends Controller
             }else{
                 $mpdf = new \Mpdf\Mpdf(['tempDir' => base_path('resources/fonts/'), 'margin_top' => 32]);
                 $mpdf->WriteHTML('
-                <div style="position: absolute; left: 55px; top: 55px;font-weight: bold; ">
+                <div style="position: absolute; left: 55px; top: 75px;font-weight: bold; ">
                     <img src="'.asset("images/logo-cathay.png").'" alt="head">
                 </div>');
                 $mpdf->WriteHTML($data['content']);
